251295e20777e53b58dbdfabac79c367bdc066ba
lupanevelina <70973228+lupanevelina@users.noreply.github.com>
2020-11-11 12:16:41 +0300

    Add files via upload

---
+++ eve1.py
@@ -0,0 +1,7 @@
+n=int(input("Dati numarul de zile : "))
+if (n==28) or (n==29) :
+    print("februarie")
+if n==30 :
+    print("aprilie","iunie","septembrie","noiembrie",sep=",")
+if n==31 :
+    print("ianuarie","martie","mai","iulie","august","octombrie","decembrie",sep=",")        
