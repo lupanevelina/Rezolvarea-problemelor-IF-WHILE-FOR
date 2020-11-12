3a8c401f1553359e487d199505b3bb92b9ff6e9f
lupanevelina <70973228+lupanevelina@users.noreply.github.com>
2020-11-12 10:19:32 +0300

    Add files via upload

--- pr7.py
+++ pr7.py
@@ -1,13 +1,12 @@
-n=int(input("Introduceti varsta lui Mihai : "))
-s=1
-s2=1
-h=0
+n=int(input("Introduceti numarul: "))
+s=0
 for i in range(1,n+1):
-    s=s*2+i
-print("Cand Mihai a implinit",n,"ani,a primit",s,"dolari")
-while s2<=100:
-    h+=1
-    s2=s2*2+h
-print("Cadoul lui Mihai a fost mai mare de 100$,când a împlinit",h,"ani")
+    p=1
+    for n in range(1,i+1):
+        p*=n
+    s+=p
+print("1!+2!+3!+...+n!=",s)
+
+
 
 
