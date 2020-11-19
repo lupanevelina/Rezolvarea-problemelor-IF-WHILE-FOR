2b43825a5f5b7a22b17e7d7e6970f382ff1e01e6
lupanevelina <70973228+lupanevelina@users.noreply.github.com>
2020-11-19 10:01:19 +0300

    Add files via upload

---
+++ pro7.py
@@ -0,0 +1,11 @@
+n=int(input("Introdu varsta lui Mihai: "))
+s=1
+s2=1
+h=0
+for i in range(1,n+1):
+    s=s*2+i
+print("Cand Mihai a implinit",n,"ani, a primit",s,"dolari")
+while s2<=100:
+    h+=1
+    s2=s2*2+h
+print("Cadoul lui Mihai a fost mai mare de 100$,când a implinit",h,"ani")
